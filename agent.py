65608eaeb479ca1e129e06ff7e1c3ee565855ce6
e2402 <e24022997b@gmail.com>
2022-11-05 16:08:55 +0200

    version1

---
+++ agent.py
@@ -0,0 +1,59 @@
+from typing import List
+
+
+class Agent:
+    def __init__(self):
+        self.options = dict()
+
+    def set_option(self, option: int, value: int):
+        self.options[option] = value
+
+    def value(self, option: int) -> float:
+        return self.options[option]
+
+
+def isParetoImprovement(agents: List[Agent], option1: int, option2: int) -> bool:
+    for i in agents:
+        if i.value(option1) < i.value(option2):
+            return False
+    return True
+
+
+def isParetoOptimal(agents: List[Agent], option: int, allOptions: List[int]) -> bool:
+    for i in allOptions:
+        if isParetoImprovement(agents, i, option) and i != option:
+            return False
+    return True
+
+
+ami = Agent()
+tami = Agent()
+rami = Agent()
+A = 1
+B = 2
+C = 3
+D = 4
+E = 5
+ami.set_option(A, 1)
+ami.set_option(B, 2)
+ami.set_option(C, 3)
+ami.set_option(D, 4)
+ami.set_option(E, 5)
+
+tami.set_option(A, 3)
+tami.set_option(B, 1)
+tami.set_option(C, 2)
+tami.set_option(D, 5)
+tami.set_option(E, 4)
+
+rami.set_option(A, 3)
+rami.set_option(B, 5)
+rami.set_option(C, 5)
+rami.set_option(D, 1)
+rami.set_option(E, 1)
+
+l = {ami, tami, rami}
+opt = {A, B, C, D, E}
+
+print(isParetoImprovement(l, B, A))
+print(isParetoOptimal(l, B, opt))
---
+++ main.py
@@ -0,0 +1,30 @@
+# This is a sample Python script.
+from agent import Agent
+
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    Ami = Agent()
+    tami = Agent()
+    rami = Agent()
+    A = 1
+    B = 2
+    C = 3
+    D = 4
+    E = 5
+    Ami.set_option(A, 1)
+    # Ami.set_option(B,2)
+    # Ami.set_option(C,3)
+    # Ami.set_option(D,4)
+    # Ami.set_option(E,5)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
